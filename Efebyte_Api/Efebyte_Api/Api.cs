aefbb0d673ea408a8e4128264de304ce3fff37ef
Carlos Leonardo Sánchez Figueroa <yo.ser.koopo@gmail.com>
2019-07-15 16:17:26 -0400

    apis

---
+++ Efebyte_Api/Efebyte_Api/Api.cs
@@ -0,0 +1,177 @@
+﻿using JWT;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Efebyte_Api
+{
+    public class Api
+    {
+        /// <summary>
+        /// Metodo para realizar una solicitud POST enviando un objeto
+        /// </summary>
+        /// <param name="objeto"> object a enviar, si es una solicitud con parametros el objeto no debe incluir arrays </param>
+        /// <param name="nombres"> array de strings con los nombres de los parametros (deben ser igual a los del objeto) </param>
+        /// <param name="url_base"> url base, Ej: http://www.ejemplo.com </param>
+        /// <param name="url"> direccion url de la consulta. Ej: /solicitud/prueba/1 </param>
+        /// <param name="token"> token de seguridad si es requerido </param>
+        /// <returns> string con la respuesta de la api </returns>
+        public async Task<string> SolicitudPostObject(object objeto, string[] nombres, string url_base, string url, string token)
+        {
+            string respuesta = "";
+            try
+            {
+                HttpClient client = new HttpClient() { BaseAddress = new Uri(url_base) };
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                if (token != null) client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + token);
+
+                HttpResponseMessage response = (nombres != null) ? await client.PostAsync(url, getParametros(objeto, nombres))
+                                                                 : (objeto != null) ? await client.PostAsJsonAsync(url, objeto) 
+                                                                                    : await client.PostAsync(url, null);
+                
+                respuesta = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception f)
+            {
+                respuesta = f.Message;
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Metodo para realizar una solicitud PUT enviando un objeto
+        /// </summary>
+        /// <param name="objeto"> object a enviar, si es una solicitud con parametros el objeto no debe incluir arrays </param>
+        /// <param name="nombres"> array de strings con los nombres de los parametros (deben ser igual a los del objeto) </param>
+        /// <param name="url_base"> url base. Ej: http://www.ejemplo.com </param>
+        /// <param name="url"> direccion url de la consulta. Ej: /solicitud/prueba/1 </param>
+        /// <param name="token"> token de seguridad si es requerido </param>
+        /// <returns> string con la respuesta de la api </returns>
+        public async Task<string> SolicitudPutObject(object objeto, string[] nombres, string url_base, string url, string token)
+        {
+            string respuesta = "";
+            try
+            {
+                HttpClient client = new HttpClient() { BaseAddress = new Uri(url_base) };
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                if (token != null)client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + token);
+                HttpResponseMessage response = (nombres != null) ? await client.PutAsync(url, getParametros(objeto, nombres)) 
+                                                                 : (objeto != null) ? await client.PutAsJsonAsync(url, objeto) 
+                                                                                    : await client.PutAsync(url, null);
+                respuesta = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception f)
+            {
+                respuesta = f.Message;
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Metodo para realizar una solicitud GET
+        /// </summary>
+        /// <param name="url_base"> url base, Ej: http://www.ejemplo.com </param>
+        /// <param name="url"> direccion url de la consulta. Ej: /solicitud/prueba/1 </param>
+        /// <param name="token"> token de seguridad si es requerido </param>
+        /// <returns> string con la respuesta de la api </returns>
+        public async Task<string> SolicitudGet(string url_base, string url, string token)
+        {
+            string respuesta = "";
+            try
+            {
+                HttpClient client = new HttpClient() { BaseAddress = new Uri(url_base) };
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                if (token != null) client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + token);
+                HttpResponseMessage response = await client.GetAsync(url);
+                respuesta = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception f)
+            {
+                respuesta = f.Message;
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Metodo para realizar una solicitud DELETE
+        /// </summary>
+        /// <param name="url_base"> url base, Ej: http://www.ejemplo.com </param>
+        /// <param name="url"> direccion url de la consulta. Ej: /solicitud/prueba/1 </param>
+        /// <param name="token"> token de seguridad si es requerido </param>
+        /// <returns> string con la respuesta de la api </returns>
+        public async Task<string> SolicitudDelete(string url_base, string url, string token)
+        {
+            string respuesta = "";
+            try
+            {
+                HttpClient client = new HttpClient() { BaseAddress = new Uri(url_base) };
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                if (token != null) client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + token);
+                HttpResponseMessage response = await client.DeleteAsync(url);
+                respuesta = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception f)
+            {
+                respuesta = f.Message;
+            }
+            return respuesta;
+        }
+
+        /// <summary>
+        /// Metodo para obtener los parametros de un objeto generico
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="nombres"></param>
+        /// <returns>FormUrlEncodedContent</returns>
+        private FormUrlEncodedContent getParametros(object o, string[] nombres)
+        {
+            List<KeyValuePair<string, string>> keys = new List<KeyValuePair<string, string>>();
+
+            foreach (string nombre in nombres)
+            {
+                PropertyInfo propertyInfo = o.GetType().GetProperty(nombre);
+
+                string text = (string)(propertyInfo.GetValue(o, null));
+
+                keys.Add(new KeyValuePair<string, string>(nombre, text));
+            }
+
+            return new FormUrlEncodedContent(keys);
+        }
+
+        /// <summary>
+        /// Metodo para desencriptar un token
+        /// </summary>
+        /// <param name="token"> string con el token </param>
+        /// <param name="key"> string con la key </param>
+        /// <returns> string con el token desencriptado </returns>
+        public static string desencriptarToken(string token, string key)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+
+                return decoder.Decode(token, key, verify: true);
+            }
+            catch (TokenExpiredException)
+            {
+                return "El Token expiro";
+            }
+            catch (SignatureVerificationException)
+            {
+                return "La firma del token es invalida";
+            }
+        }
+    }
+}
---
+++ Efebyte_Generals/Efebyte_Generals/Generals.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efebyte_Generals
+{
+    public static class Generals
+    {   
+        /// <summary>
+        /// Metodo para validar un rut
+        /// </summary>
+        /// <param name="rut"> string con el rut </param>
+        /// <returns> true si es valido, false si no es valido </returns>
+        public static bool validarRut(string rut)
+        {
+            string rutF = formatearRut(rut);
+
+            bool validacion = false;
+            try
+            {
+                rutF = rutF.ToUpper();
+                rutF = rutF.Replace(".", "");
+                rutF = rutF.Replace("-", "");
+                int rutAux = int.Parse(rutF.Substring(0, rutF.Length - 1));
+
+                char dv = char.Parse(rutF.Substring(rutF.Length - 1, 1));
+
+                int m = 0, s = 1;
+                for (; rutAux != 0; rutAux /= 10)
+                {
+                    s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+                }
+                if (dv == (char)(s != 0 ? s + 47 : 75))
+                {
+                    validacion = true;
+                }
+            }
+            catch (Exception) { }
+
+            return validacion;
+        }
+
+        /// <summary>
+        /// Metodo para dar formato a un rut
+        /// </summary>
+        /// <param name="rut"> string con el rut </param>
+        /// <returns> string con el rut formateado </returns>
+        public static string formatearRut(string rut)
+        {
+            int cont = 0;
+            string format;
+            if (rut.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                format = "-" + rut.Substring(rut.Length - 1);
+                for (int i = rut.Length - 2; i >= 0; i--)
+                {
+                    format = rut.Substring(i, 1) + format;
+                    cont++;
+                    if (cont == 3 && i != 0)
+                    {
+                        format = "." + format;
+                        cont = 0;
+                    }
+                }
+                return format;
+            }
+        }
+
+        /// <summary>
+        /// Metodo para crear archivo de logs y escribir logs en el
+        /// </summary>
+        /// <param name="ruta"> string con la ruta donde se creara la carpeta del projecto </param>
+        /// <param name="nombreProjecto"> string con el nombre del projecto </param>
+        /// <param name="msg"> string con el mensaje del log </param>
+        /// <param name="lugar"> string con el lugar del incidente </param>
+        public static void logs(string ruta, string nombreProjecto, string msg, string lugar)
+        {
+            string carpetaEfeDoc = $"{ruta}\\{nombreProjecto}";
+            string carpeta = $"{carpetaEfeDoc}\\Log";
+            if (!Directory.Exists(carpetaEfeDoc))
+            {
+                Directory.CreateDirectory(carpetaEfeDoc);
+            }
+            if (!Directory.Exists(carpeta))
+            {
+                Directory.CreateDirectory(carpeta);
+            }
+
+            string archivo = $"{carpeta}\\logs.txt";
+            string error = $"[{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")}] Error: '{msg}', Lugar: '{lugar}'";
+            if (!File.Exists(archivo))
+            {
+                File.Create(archivo);
+                TextWriter tw = new StreamWriter(archivo);
+                tw.WriteLine(error);
+                tw.Close();
+            }
+            else
+            {
+                using (var tw = new StreamWriter(archivo, true))
+                {
+                    tw.WriteLine(error);
+                }
+            }
+        }
+
+
+
+    }
+}
